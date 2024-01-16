1e99dba1be5b08319ae9364be47a6a3df7f0abde
quid_x <quid@hanpass.com>
2024-01-16 13:07:57 +0900

    framework hexagon

--- start/src/main/kotlin/com/quid/start/router/RouterViewCLIAdaptor.kt
+++ start/src/main/kotlin/com/quid/start/router/RouterViewCLIAdaptor.kt
@@ -3,14 +3,12 @@ package com.quid.start.router
 import com.quid.start.router.application.RouterViewUseCase
 import com.quid.start.router.application.RouterViewUseCase.RouterViewInputPort
 import com.quid.start.router.domain.Router
+import com.quid.start.router.domain.RouterType
 
 class RouterViewCLIAdaptor(
     private val routerViewUseCase: RouterViewUseCase = RouterViewInputPort(RouterViewFileAdaptor())
 ) {
 
-    fun obtainRelatedRouters(type: String): List<Router> {
-        return routerViewUseCase.getRouters(
-            outer.filterRouterByType(type)
-        )
-    }
+    fun obtainRelatedRouters(type: String): List<Router> =
+        routerViewUseCase.getRouters(Router.filterByType(RouterType.valueOf(type)))
 }
